981a0c6162e689c36cfa0c4e3e78e58ed267b7ec
Rocky Rhodes <rocky@google.com>
2012-06-01 12:50:31 -0700

    Enable ram console.
    After a warm reboot, /proc/last_kmsg will show the kernel log
    from the previous session.
    
    Change-Id: Ia3f806d0924513a9ed75f49cf53818898579fb11

--- arch/arm/mach-omap2/board-notle.c
+++ arch/arm/mach-omap2/board-notle.c
@@ -90,6 +90,7 @@
 
 #include "timer-gp.h"
 #include "omap4_ion.h"
+#include "omap_ram_console.h"
 #include "hsmmc.h"
 #include "control.h"
 #include "pm.h"
@@ -2486,6 +2487,9 @@ static void __init notle_map_io(void)
 
 static void __init notle_reserve(void)
 {
+        omap_ram_console_init(OMAP_RAM_CONSOLE_START_DEFAULT,
+                              OMAP_RAM_CONSOLE_SIZE_DEFAULT);
+
         /* do the static reservations first */
         memblock_remove(PHYS_ADDR_SMC_MEM, PHYS_ADDR_SMC_SIZE);
         memblock_remove(PHYS_ADDR_DUCATI_MEM, PHYS_ADDR_DUCATI_SIZE);
