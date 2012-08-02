5e4a71705a929bc3e3beae7d8546be0655d6e0a7
Corey Tabaka <eieio@google.com>
2012-08-02 11:00:04 -0700

    Reenable shutdown on BAT_VLOW interrupt.
    Change-Id: Ic19ee57b1b891f29cfdbdb1e3bca1d65d675d166

--- drivers/mfd/twl6030-irq.c
+++ drivers/mfd/twl6030-irq.c
@@ -268,7 +268,7 @@ static irqreturn_t handle_twl6030_vlow(int irq, void *unused)
 	pr_err("twl6030: BAT_VLOW interrupt; threshold=%dmV\n",
 	       2300 + (vbatmin_hi_threshold - 0b110) * 50);
 
-#if 1 /* temporary */
+#if 0 /* temporary */
 	pr_err("%s: disabling BAT_VLOW interrupt\n", __func__);
 	disable_irq_nosync(twl6030_irq_base + TWL_VLOW_INTR_OFFSET);
 	WARN_ON(1);
