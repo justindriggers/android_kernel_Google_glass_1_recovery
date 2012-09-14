4d650288391e31929ad0d63d966351f03914c4e2
Chris Manton <cmanton@google.com>
2012-09-14 11:14:06 -0700

    Add counter for suspend-resume cycles
    Change-Id: I4b5cf612862d9be32e8002c8e9596d797d940424

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -27,6 +27,17 @@
 
 #include "power.h"
 
+static unsigned int suspend_cnt = 0;
+
+unsigned int get_suspend_cnt(void) {
+	return suspend_cnt;
+}
+EXPORT_SYMBOL(get_suspend_cnt);
+
+static void inc_suspend_cnt(void) {
+	suspend_cnt++;
+}
+
 const char *const pm_states[PM_SUSPEND_MAX] = {
 #ifdef CONFIG_EARLYSUSPEND
 	[PM_SUSPEND_ON]		= "on",
@@ -223,6 +234,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	if (suspend_test(TEST_DEVICES))
 		goto Recover_platform;
 
+	inc_suspend_cnt();
 	error = suspend_enter(state);
 
  Resume_devices:
