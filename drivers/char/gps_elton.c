9ed4255bc734a1beb10cb4c3a2530f6231151373
Chris Manton <cmanton@google.com>
2012-10-29 09:27:51 -0700

    Added atomic interrupt counting for EVT1.x GPS awake detection
    Change-Id: I3e13c7752b09f151ae85840fcb02554ba68c9f18

--- drivers/char/gps_elton.c
+++ drivers/char/gps_elton.c
@@ -54,7 +54,7 @@ struct gps_elton_data_s {
 	// EVT1.x Workqueue check to poll for uart interrupts.
 	struct delayed_work work_check;
 	// EXT1.x Number of uart interrupts to infer if GPS is awake or not.
-	int uart_int_cnt;
+	atomic_t uart_int_cnt;
 	// EVT1.x Used to read irq statistics to determine if chip is awake.
 	int irq_num;
 };
@@ -84,10 +84,21 @@ static ssize_t gps_elton_awake_store(struct device *dev,
                                      struct device_attribute *attr,
                                      const char *buf, size_t count);
 
+static ssize_t gps_elton_int_offset_show(struct device *dev,
+                                    struct device_attribute *attr, char *buf);
+
+// Set the wake state of the GPS chip.
+static ssize_t gps_elton_int_offset_store(struct device *dev,
+                                     struct device_attribute *attr,
+                                     const char *buf, size_t count);
+
 static struct device_attribute attrs[] = {
 	__ATTR(awake, 0666,
 	       gps_elton_awake_show,
 	       gps_elton_awake_store),
+	__ATTR(int_offset, 0666,
+	       gps_elton_int_offset_show,
+	       gps_elton_int_offset_store),
 };
 
 // Determines if device is EVT 1.x or not.
@@ -126,7 +137,8 @@ static void gps_elton_work_task(struct work_struct *work)
 {
 	struct delayed_work *dw = to_delayed_work(work);
 	struct gps_elton_data_s *gps_elton_data = container_of(dw, struct gps_elton_data_s, work_check);
-	int uart_int_cnt;
+	int uart_int_cnt_curr;
+	int uart_int_cnt_last;
 
 	if (-1 == gps_elton_data->irq_num) {
 		dev_err(&gps_elton_data->pdev->dev,
@@ -138,16 +150,17 @@ static void gps_elton_work_task(struct work_struct *work)
 	// If the interrupt count hasn't changed since last time, assume we are off.
 	// Need to make sure the polling time is long enough to allow interrupts to change
 	// in between.
-	uart_int_cnt = _get_interrupt_count(gps_elton_data->irq_num);
-	if (gps_elton_data->uart_int_cnt == uart_int_cnt) {
+	uart_int_cnt_curr = _get_interrupt_count(gps_elton_data->irq_num);
+	uart_int_cnt_last = atomic_read(&gps_elton_data->uart_int_cnt);
+	if (uart_int_cnt_last >= uart_int_cnt_curr) {
 		dev_info(&gps_elton_data->pdev->dev, "EVT 1.x Device was determined to be in hibernation\n");
 		gps_elton_data->is_awake = GPS_STATE_HIBERNATE;
 	} else {
 		dev_info(&gps_elton_data->pdev->dev, "EVT 1.x Device was determined to be awake\n");
 		gps_elton_data->is_awake = GPS_STATE_AWAKE;
+		// Update the uart interrupt count for next time.
+		atomic_set(&gps_elton_data->uart_int_cnt, uart_int_cnt_curr);
 	}
-	// Update the uart interrutpt count for next time.
-	gps_elton_data->uart_int_cnt = uart_int_cnt;
 
 	// Put us back on the work queue.
 	schedule_delayed_work(&gps_elton_data->work_check,
@@ -218,7 +231,7 @@ static ssize_t gps_elton_awake_store(struct device *dev,
 		_toggle_power(gps_elton_data);
 		if (_is_evt_1_x(gps_elton_data)) {
 			/* EVT 1.x - Force cache irq count to not equal system irq count. */
-			gps_elton_data->uart_int_cnt = -1;
+			atomic_set(&gps_elton_data->uart_int_cnt, -1);
 			/* EVT1.x */
 			gps_elton_data->is_awake = GPS_STATE_AWAKE;
 		}
@@ -241,7 +254,7 @@ static ssize_t gps_elton_awake_store(struct device *dev,
 		_toggle_power(gps_elton_data);
 		if (_is_evt_1_x(gps_elton_data)) {
 			/* EVT 1.x - Force cache irq count to be equal to system irq count. */
-			gps_elton_data->uart_int_cnt = _get_interrupt_count(gps_elton_data->irq_num);
+			atomic_set(&gps_elton_data->uart_int_cnt,  _get_interrupt_count(gps_elton_data->irq_num));
 			/* EVT1.x */
 			gps_elton_data->is_awake = GPS_STATE_HIBERNATE;
 		}
@@ -250,6 +263,41 @@ static ssize_t gps_elton_awake_store(struct device *dev,
 
 	gps_elton_data->valid_pulse_in_jiffies = jiffies_current + msecs_to_jiffies(TIME_BETWEEN_AWAKE_PULSES_IN_MS);
 	return count;
+}
+
+static ssize_t gps_elton_int_offset_show(struct device *dev,
+                                         struct device_attribute *attr, char *buf)
+{
+	struct gps_elton_data_s *gps_elton_data = dev_get_drvdata(dev);
+	if (_is_evt_1_x(gps_elton_data)) {
+		// EVT 1.x
+		return snprintf(buf, PAGE_SIZE, "%d\n", atomic_read(&gps_elton_data->uart_int_cnt));
+	} else {
+		// EVT 2.0
+		return -ENODEV;
+	}
+}
+
+/* User manually specifying suspend or resume mode of GPS chip. */
+static ssize_t gps_elton_int_offset_store(struct device *dev,
+                                          struct device_attribute *attr,
+                                          const char *buf, size_t count)
+{
+	int data;
+	int old_cnt;
+	struct gps_elton_data_s *gps_elton_data = dev_get_drvdata(dev);
+	sscanf(buf, "%d", &data);
+
+	old_cnt = atomic_read(&gps_elton_data->uart_int_cnt);
+	if (_is_evt_1_x(gps_elton_data)) {
+		atomic_add(data, &gps_elton_data->uart_int_cnt);
+		dev_info(gps_elton_data->dev, "%s Adding interrupt count old:%d new:%d cnt:%d\n",
+		         __func__, old_cnt, atomic_read( &gps_elton_data->uart_int_cnt), data);
+		return count;
+	} else {
+		// EVT 2.0
+		return -ENODEV;
+	}
 }
 
 static void gps_elton_early_suspend(struct early_suspend *h)
@@ -303,7 +351,7 @@ static int gps_elton_suspend(struct platform_device *pdev, pm_message_t state) {
 			_toggle_power(gps_elton_data);
 			gps_elton_data->is_awake = GPS_STATE_HIBERNATE;
 			/* EVT 1.x - Force cache irq count to be equal to system irq count. */
-			gps_elton_data->uart_int_cnt = _get_interrupt_count(gps_elton_data->irq_num);
+			atomic_set(&gps_elton_data->uart_int_cnt, _get_interrupt_count(gps_elton_data->irq_num));
 			was_awake = 1;
 		}
 	} else {
@@ -380,6 +428,7 @@ static int gps_elton_probe(struct platform_device *pdev) {
 	 * EVT2 uses a direct line from the GPS chip to indicate awake.
 	 */
 	gps_elton_data->is_awake = GPS_STATE_UNKNOWN;
+	atomic_set(&gps_elton_data->uart_int_cnt, 0);
 
 	/* Set up sysfs device attributes. */
 	for (attr_count = 0; attr_count < ARRAY_SIZE(attrs); attr_count++) {
